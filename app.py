df8df4a654f9652136237bd997caf3dc62f86891
2tinz <eridiongj@gmail.com>
2023-05-18 20:13:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def main():
        [Kaggle](https://www.kaggle.com/avikumart) 
        """)
        a,b,c = st.columns([0.2,0.6,0.2])
- with a: 
+ with c: 
         st.image("banner-picture.jpeg", use_column_width=True)
 
 
